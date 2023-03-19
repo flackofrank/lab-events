0e963fa1ff161881032849ada0a03778e1cc4bc2
Franklin Ramirez <ramirezfrankie2251@gmail.com>
2023-03-19 12:43:50 -0400

    Lab Submission

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+let colorButtons = document.querySelectorAll(".color")
+let currentColorSquare = document .querySelector("#current-color")
+
+for (const colorButton of colorButtons){ 
+  colorButton.addEventListener("click", (event) => {
+    currentColorSquare.style.background = event.target.style.background
+  })
+}
+
+let colorCells = document.querySelectorAll(".cell")
+
+for (const cell of colorCells){
+  cell.addEventListener("click", event => {
+    cell.style.background = currentColorSquare.style.background
+  })
+}
